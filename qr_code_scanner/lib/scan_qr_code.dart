3dca8f6f7f8102fd6689d2fbb16490c9cbefba8f
arpr <andyrenework@gmail.com>
2025-01-12 02:56:21 +0300

    The tutorial has erros, i am coding in VS code. I am not sure if i will finish this app. Probably i will.

--- qr_code_scanner/lib/scan_qr_code.dart
+++ qr_code_scanner/lib/scan_qr_code.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/flutter_barcode_scanner.dart';
 
 class ScanQrCode extends StatefulWidget {
   const ScanQrCode({Key? key}) : super(key: key);
@@ -8,8 +10,31 @@ class ScanQrCode extends StatefulWidget {
 }
 
 class _ScanQrCodeState extends State<ScanQrCode> {
+  String qrResult = 'Scanned Data will appear here';
+  Future<void> scanQR()async{
+    try{
+      final qrCode = await FlutterBarcodeScanner.scanBarcode('#ff6666', 'Cancel', true, ScanMode.QR);
+
+    }on PlatformException{
+      qrResult = 'Fail to get QR code';
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text('QR Code Scanner'))
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30,),
+            Text('$qrResult',style: TextStyle(color: Colors.black),),
+            SizedBox(height: 30,),
+            ElevatedButton(onPressed:scanQR, child: Text('Scan code'),),
+          ]
+        ),
+      ),
+    );
   }
 }
